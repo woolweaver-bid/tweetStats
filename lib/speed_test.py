ec3b6f706a55ca77c34834b890ca284893565f01
Michael Woolweaver <mwoolweaver@users.noreply.github.com>
2019-11-24 16:54:49 -0600

    better ipstack debugging

--- lib/speed_test.py
+++ lib/speed_test.py
@@ -21,7 +21,9 @@ def check_ipstack():
 
     if status_code == 101:
         print("invalid access key \nipstack API URL\n" + address)
-    else if status_code != None
+    elif status_code != 101 & badip == None
+        print("Please check your IP addres \nipstack API URL\n" + address)
+    else
         print(str(success) + "\nipstack API URL\n" + address)
 
 def speedtest_ip():
