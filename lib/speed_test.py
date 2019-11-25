c768198c7021768421942f000f002a84371a754e
Michael Woolweaver <mwoolweaver@users.noreply.github.com>
2019-11-24 20:09:28 -0600

    one day ill actually learn how to python properly

--- lib/speed_test.py
+++ lib/speed_test.py
@@ -10,7 +10,7 @@ def check_ipstack():
     key = cfgIP()
     ip = get('https://www.wikipedia.org').headers['X-Client-IP']
     address = "http://api.ipstack.com/" + ip + "?access_key=" + key + "&output=json&fields=region_name,continent_name"
-    url = get(address)
+    url = get(address).json()
 
     success = url.status_code
 
