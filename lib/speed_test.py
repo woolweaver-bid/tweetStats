145383a3bd1c58b54c95af8366592b32a4e92508
Michael Woolweaver <m.woolweaver@icloud.com>
2019-11-26 09:50:28 -0600

    Update speed_test.py

--- lib/speed_test.py
+++ lib/speed_test.py
@@ -34,13 +34,7 @@ def speedtest_ip():
     import speedtest
 
     servers = []
-    # If you want to test against a specific server
-    # servers = [1234]
-
     threads = None
-    # If you want to use a single threaded test
-    # threads = 1
-
     s = speedtest.Speedtest()
     s.get_servers(servers)
     s.get_best_server()
