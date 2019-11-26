5a5b7c6dd1422241b9e167a9c7c7fbf36b1ebde1
Michael Woolweaver <m.woolweaver@icloud.com>
2019-11-26 09:23:13 -0600

    check_ipstack() renamed to ipstack()

--- lib/speed_test.py
+++ lib/speed_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def check_ipstack():
+def ipstack():
 
     from lib.get_config import get_cfgip as cfgIP
 
@@ -76,7 +76,7 @@ def speedtest_ip():
     dlMBs = str(dlMB) + " MB"
     Sdata = ulMBs + "/" + dlMBs
 
-    cip = check_ipstack()
+    cip = ipstack()
     ip = '.'.join(cip[1].split('.')[:2]) + '.xx.xx'
     ipstack = cip[0]
     region = ipstack['region_name']
