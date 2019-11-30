f6e1e9742e947a15f92babde20ce00260d9428b2
Michael Woolweaver <m.woolweaver@icloud.com>
2019-11-30 09:35:38 -0600

    Update pihole_info.py

--- lib/pihole_info.py
+++ lib/pihole_info.py
@@ -26,7 +26,7 @@ def pihole_info():
     rp = reach_pihole()[0]
 
     # d = rp.json()
-    gla = d["gravity_last_updated"]
+    gla = rp["gravity_last_updated"]
 
     if not all(k in d for k in # check for needed variables
                ("domains_being_blocked", "dns_queries_today", "ads_blocked_today", "ads_percentage_today", "queries_forwarded", "queries_cached", "unique_clients", "privacy_level", "gravity_last_updated")):
