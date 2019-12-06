e7cf6e631b7044c26ce920b88148322ff74afca2
Michael Woolweaver <m.woolweaver@icloud.com>
2019-12-05 23:35:13 -0600

    Update sys_info.py

--- lib/sys_info.py
+++ lib/sys_info.py
@@ -21,8 +21,8 @@ def sys_info():
     percentHDD = round(100 * float(GBused)/float(GBtotal), 2) # create percentage for disk used
     cpuLoadAvg = str(gl()) # regex can't manipulate a list so turn to string
     netfaces = str(ni.interfaces()) # regex can't manipulate a list so turn to string
-    usedMem = str(sz(vm()[2])) # RAM used
-    totMem = str(sz(vm()[1])) #  RAM available
+    usedMem = str(sz(vm()[4])) # RAM used
+    totMem = str(sz(vm()[2])) #  RAM available
     percentMem = str(vm()[3]) # RAM percentage used
     totalGB = sz(total) # total disk space in GB
     usedGB = sz(used) # total disk space used in GB
